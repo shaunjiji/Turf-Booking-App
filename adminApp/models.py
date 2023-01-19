2e2aae0392adce430578a8f3d19ccf7153f1fe0b
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-19 15:10:35 +0530

    create first draft of category, manager, turf models

--- adminApp/models.py
+++ adminApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+#manager table, category table, turf table
+
+class Categorydb(models.Model):
+    name = models.CharField(max_length=100)
+
+class Managerdb(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Turfdb(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image =  models.ImageField(upload_to='turf')
+    price = models.IntegerField() 
+    location = models.CharField(max_length=100)
+    categoryid = models.ForeignKey(Categorydb, on_delete=CASCADE)
+    managerid = models.ForeignKey(Managerdb, on_delete=CASCADE)
+
