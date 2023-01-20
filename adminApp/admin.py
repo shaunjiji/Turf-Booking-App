fddbec7f78a9e7af34e3a7f2e26c4f150fb65a61
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-20 14:27:44 +0530

    create templates to view/add categories

--- adminApp/admin.py
+++ adminApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from . models import *
 
-# Register your models here.
 
 
-admin.site.register(Categorydb, Managerdb, Turfdb)
+
+admin.site.register(Managerdb)
+admin.site.register(Categorydb)
+admin.site.register(Turfdb)
