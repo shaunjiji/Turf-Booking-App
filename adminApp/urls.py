20fa6de19f678853e2382bde03a9fee96faf9fd1
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-20 13:56:27 +0530

    add route to view all categories

--- adminApp/urls.py
+++ adminApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.getDashboard, name='dashboard')
+    path('dashboard/', views.getDashboard, name='dashboard'),
+    path('categories/', views.viewCategories, name='view-categories')
 ]
+
+##routes for category, view all categories, add category, delete category, edit category?   
--- adminApp/views.py
+++ adminApp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . models import *
 
 # Create your views here.
 
 def getDashboard(request):
     return render(request, 'index.html')
+
+def viewCategories(request):
+    data = Categorydb.objects.all()
+    return render(request, 'view-categories.html', {'data': data} )
+
