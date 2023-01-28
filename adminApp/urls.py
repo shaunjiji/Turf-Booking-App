9e788f30df54232c41c3f94f753ae2935f60ba36
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-28 14:35:06 +0530

    create path for addManager

--- adminApp/urls.py
+++ adminApp/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('delete-category/<int:id>', views.deleteCategory, name='deleteCategory'),
     path('update-category/<int:id>', views.updateCategory, name='updateCategory'),
     path('managers/', views.viewManagers, name='viewManagers'),
-    path('delete-manager/<int:id>', views.deleteManager, name='deleteManager')
+    path('delete-manager/<int:id>', views.deleteManager, name='deleteManager'),
+    path('add-manager/', views.addManager, name='addManager')
 ]
  
  
