8e0b8c17c5fae50f009d940d491a58b6a53d7052
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-29 10:56:07 +0530

    create html page for all turfs

--- adminApp/models.py
+++ adminApp/models.py
@@ -23,4 +23,3 @@ class Turfdb(models.Model):
     location = models.CharField(max_length=100)
     categoryid = models.ForeignKey(Categorydb, on_delete=CASCADE)
     managerid = models.ForeignKey(Managerdb, on_delete=CASCADE)
-
