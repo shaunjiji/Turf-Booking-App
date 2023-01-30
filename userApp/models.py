9adc8f4189983a475fc69f7946a7f01fc6c071ec
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-30 16:36:44 +0530

    create user and booking db

--- userApp/models.py
+++ userApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from managerApp.models import Turfdb 
 
 # Create your models here.
+##user table, login, password
+##bookings table
+##
+
+class Userdb(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+
+class Bookingdb(models.Model):
+    userid = models.ForeignKey(Userdb, on_delete=CASCADE)
+    turfid = models.ForeignKey(Turfdb, on_delete=CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
