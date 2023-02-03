12391e9b47ed057b7a941d64e9476d7acc0b4ea1
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-03 12:35:21 +0530

    create method to view all turfs

--- userApp/urls.py
+++ userApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/',views.getHome, name='getHome')
+    path('home/',views.getHome, name='home')
 
 ]
  
--- userApp/views.py
+++ userApp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from . models import *
 
 # Create your views here.
 
 def getHome(request):
     return render(request,'home.html')
+
+def viewTurfs(request):
+    data = Turfdb.objects.all()
+    return render(request, 'turfs.thml', {'data': data})
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
