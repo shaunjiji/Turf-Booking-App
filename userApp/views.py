f3bbf1c130837322f631dc233f90991f20ced4f5
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-07 11:49:18 +0530

    create signup form

--- userApp/views.py
+++ userApp/views.py
@@ -13,8 +13,7 @@ def viewTurfs(request):
     return render(request, 'turfs.html', {'data': data})
 
 def signup(request):
-    form = UserCreationForm()   
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'signup.html')
 
 def login(request):
     return render (request, 'login.html')
