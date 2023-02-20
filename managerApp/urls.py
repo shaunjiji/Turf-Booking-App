64871ee868286b1ccd84846d27829781f9ddb014
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-20 08:31:33 +0530

    create template/static folder;implement login_view method and path

--- managerApp/urls.py
+++ managerApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('login/', views.login, name='login')
 ]
  
  
--- managerApp/views.py
+++ managerApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def manager_login_view(request):
+    return render ('manager-index.html')
