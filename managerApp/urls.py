2e7bf7aa839db964490d74237c7e6448d5756e5d
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-21 08:24:49 +0530

    add login method for managers

--- managerApp/urls.py
+++ managerApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('login/', views.manager_view, name='login_view')
 ]
  
  
--- managerApp/views.py
+++ managerApp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from adminApp.models import *
 
 # Create your views here.
 
-def manager_login_view(request):
-    return render ('manager-index.html')
+def manager_view(request):
+    return render (request,'manager-login.html')
+
+def manager_login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        if Managerdb.objects.filter(email=email,password=password).exists():
+            data = Managerdb.objects.filter(email=email, password=password).values('name', 'image').first()
+            request.session['name'] = data['name']
+            request.session['managerid'] = data['id']
+            request.session['username'] = data['username']
+            request.session['password'] = data['password']
