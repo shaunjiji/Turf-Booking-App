de6c09fe78c157db1a46d57e7b1baa2d3783bbc1
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-21 08:53:49 +0530

    Add path for requests

--- managerApp/urls.py
+++ managerApp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.manager_view, name='login_view'),
-    path('login_manager/',views.manager_login, name='manager-login')
+    path('login_manager/',views.manager_login, name='manager-login'),
+    path('request/', views.manager_requests, name='requests')
 ]
  
  
